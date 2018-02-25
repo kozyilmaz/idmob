ce9e6a8d022bd18c4801c885225d0411ecf9e1ae
mehmet.dogan <mehmetdogan1993@gmail.com>
2018-02-25 14:29:55 +0300

    comment added

--- iotdatamarket.sol
+++ iotdatamarket.sol
@@ -36,31 +36,30 @@ contract iotdatamarket {
     mapping(address => customer) private customer_map;
     address[] private vendor_arr;
 
-    function query_sensor (uint sensor_type, uint index) public view returns (address result) {
-        if (vendor_map[vendor_arr[index]].types[sensor_type] != true || vendor_map[vendor_arr[index]].payloads[sensor_type].length == 0) {
-            // TODO check, if doesnt work return 0xdeadbeef
-            revert();
-        }
-        return vendor_arr[index];
-    }
-
-
+    /// @dev Registers a vendor to marketplace to be able to list their sensor_data_push
+    /// @param prefix Symbol for vendor which is shown on marketplace
+    /// @param sensors list of sensor types vendor will publish on marketplace
+    /// @param costs list of prices corresponding to each sensor
+    /// @return result succesfull registered or not 
     function vendor_register (string prefix, uint[] sensors, uint[] costs) public returns (bool result) {
-        /* check if vendor is already registered */
+        // check if vendor is already registered 
         if (bytes(vendor_map[msg.sender].prefix).length != 0) {
             return false;
         }
-        /* add to vendor object to "vendor map" */
+        // add to vendor object to "vendor map" 
         vendor_map[msg.sender].prefix = prefix;
         for (uint it = 0; it < sensors.length; it++) {
             vendor_map[msg.sender].types[sensors[it]] = true;
             vendor_map[msg.sender].prices[sensors[it]] = costs[it];
         }
-        /* add vendor address to "vendor array" */
+        // add vendor address to "vendor array" 
         vendor_arr.push(msg.sender);
         return true;
     }
 
+    /// @dev Add devices address that can push data on behalf of vendor
+    /// @param device_address Address of the device that allowed to push data 
+    /// @return true if address is valid for vendor
     function add_valid_device (address device_address) public returns (bool) {
         // returns false if device is already added
         if (vendor_map[msg.sender].devices[device_address] == true) {
@@ -70,14 +69,26 @@ contract iotdatamarket {
         return true;
     }
 
+    /// @dev Get total vendors registered on market
+    /// @result returns total count of vendors 
     function vendor_length () public view returns (uint length) {
         return vendor_arr.length;
     }
 
+    /// @dev Get symbol of the vendor to be listed
+    /// @param addr Address of the vendor 
+    /// @return prefix corresponding symbol to vendor's address
     function get_vendor (address addr) public view returns (string prefix) {
         return (vendor_map[addr].prefix);
     }
 
+    /// @dev Adds the data to be shown on market
+    /// @param vendor_address Address of the vendor 
+    /// @param sensor_type Represents data belongs to which sensor type
+    /// @param schema JSON representation of schema representation of data
+    /// @param timestamp Time when data is pushed on swarm
+    /// @param spatial Geolocation of device where the data is pushed
+    /// @param swarm url of the content on swarm  
     function sensor_data_push (address vendor_address, uint sensor_type, string schema, uint timestamp, string spatial, string swarm) public returns (bool result) {
         if (vendor_map[vendor_address].types[sensor_type] != true) {
             return false;
@@ -90,6 +101,15 @@ contract iotdatamarket {
         vendor_map[vendor_address].payloads[sensor_type].push(payload(timestamp,swarm,schema,spatial));
         return true;
     }
+
+    /// @dev Checks whether vendor has sensor_type or not, returns address if vendor has
+    /// @param sensor_type Numeric representation of corresponding sensor_type
+    /// @param index iteration number for vendor_list
+    function query_sensor (uint sensor_type, uint index) public view returns (address result) {
+        require(!vendor_map[vendor_arr[index]].types[sensor_type] || vendor_map[vendor_arr[index]].payloads[sensor_type].length == 0);
+        return vendor_arr[index];
+    }
+
 
     function sensor_data_pull (address vendor_address, uint sensor_type, uint index) public view returns (string schema, uint timestamp, string spatial, uint price) {
         return (vendor_map[vendor_address].payloads[sensor_type][index].schema,
@@ -161,15 +181,5 @@ contract iotdatamarket {
         }
     }
 
-    /*
-        function query_sensor_length (uint sensor_type) public view returns (uint vcount) {
-        uint vendor_count = 0;
-        for (uint it = 0; it < vendor_arr.length; it++) {
-            if (vendor_map[vendor_arr[it]].types[sensor_type] == true && vendor_map[vendor_arr[it]].payloads[sensor_type].length>0) {
-               vendor_count++;
-            }
-        }
-        return vendor_count;
-    }
-    */
+    
 }
